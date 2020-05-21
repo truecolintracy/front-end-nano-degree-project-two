4075caf8db3b9614727e401e498de22bc4aa486e
Colin Tracy <colin@aiselng.com>
2020-05-21 00:11:41 -0600

    Update nav building

--- js/app.js
+++ js/app.js
@@ -17,15 +17,34 @@
  * Define Global Variables
  * 
 */
-
-
+const SECTIONS = document.querySelectorAll('[data-nav]');
+const EMPTY_NAV = document.getElementById('navbar__list');
+const NAV = ['Home', 'First Section', 'Second Section', 'Third Section', 'Fourth Section'];
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function buildLink(str) {
+  const link = str.replace(/\s+/g, '-').toLowerCase();
+  return `#${link}`;
+}
 
+function buildList(item) {
+  const LIST_ITEM = document.createElement('li');
+  const HREF_ITEM = document.createElement('a');
+  HREF_ITEM.setAttribute('href', buildLink(item));
+  HREF_ITEM.textContent = item;
+  LIST_ITEM.appendChild(HREF_ITEM);
+  return LIST_ITEM;
+}
 
+function insertDOMElements(parent, children) {
+  children.forEach(function (child) {
+
+    parent.appendChild(buildList(child));
+  })
+}
 
 /**
  * End Helper Functions
@@ -34,7 +53,7 @@
 */
 
 // build the nav
-
+insertDOMElements(EMPTY_NAV, NAV);
 
 // Add class 'active' to section when near top of viewport
 
